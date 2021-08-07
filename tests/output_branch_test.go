b4a8921fa5e742866835f59f4574e1c14b73d1e5
iamthen0ise <thefiltdj@gmail.com>
2021-08-07 20:09:13 +0300

    Issue #7. Enforce Downcase

--- tests/output_branch_test.go
+++ tests/output_branch_test.go
@@ -12,21 +12,21 @@ func TestBuildBranchName(t *testing.T) {
 	gitBranchName.IssueID = "JIRA-123"
 	gitBranchName.BuildBranchName()
 
-	want := "JIRA-123"
+	want := "jira-123"
 	if gitBranchName.BranchName != want {
 		t.Errorf("want %v got %v", want, gitBranchName.BranchName)
 	}
 
 	gitBranchName.Prefix = "feature"
 	gitBranchName.BuildBranchName()
-	want = "feature/JIRA-123"
+	want = "feature/jira-123"
 	if gitBranchName.BranchName != want {
 		t.Errorf("want %v got %v", want, gitBranchName.BranchName)
 	}
 
 	gitBranchName.CustomTextParts = append(gitBranchName.CustomTextParts, "Some", "text", "i", "want", "to", "add")
 	gitBranchName.BuildBranchName()
-	want = "feature/JIRA-123-Some-text-i-want-to-add"
+	want = "feature/jira-123-some-text-i-want-to-add"
 	if gitBranchName.BranchName != want {
 		t.Errorf("want %v got %v", want, gitBranchName.BranchName)
 	}
